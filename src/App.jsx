9ef609763a9ca32ed09a130cd06249917cd360ad
Rishith Kumar <rishithkumar830@gmail.com>
2025-11-20 06:59:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const Hello = (props) => {
+    const birthYear = () => {
+        return new Date()   .getFullYear() - props.age
+    }
+    return (
+        <h1>Hello {props.name}</h1>
+
+    )
+}
+
+
+
+const App = () => {
+  return (
+    <>
+        <h1>Hello There</h1>
+        <Hello name="Rishith"/>
+    </>
+  )
+}
+
+export default App
